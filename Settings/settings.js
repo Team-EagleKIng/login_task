f6edee692206afcb81b94439a66b40ed9abf7b08
Okprimee <ouwede@terragonltd.com>
2019-09-15 23:41:07 +0100

    modified setting.js

--- Settings/settings.js
+++ Settings/settings.js
@@ -2,10 +2,8 @@ const mongoose = require('mongoose');
 const config = require('../Config/config');
 mongoose.set('useCreateIndex', true);
 
-// mongodb+srv://survey:testsurvey@cluster0-wpxhp.mongodb.net/test?retryWrites=true
 
 mongoose.Promise = global.Promise
-// const connectionUrl = `mongodb://karo:password123@ds137703.mlab.com:37703/survey`
 const connectionUrl = `mongodb://${config.mongo.connection.host}:${config.mongo.connection.port}/${config.mongo.connection.db}` || "mongo"
 console.log('Connecting to Mongo DB on ', connectionUrl);
 mongoose.connect(connectionUrl, {useNewUrlParser: true})
